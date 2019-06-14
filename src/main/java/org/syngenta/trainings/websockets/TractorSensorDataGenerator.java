d7352d61379658142bf54df21f122e68543b3b28
Taras <murzenkovtn@gmail.com>
2019-06-14 16:31:14 +0300

    improvements

--- src/main/java/org/syngenta/trainings/websockets/TractorSensorDataGenerator.java
+++ src/main/java/org/syngenta/trainings/websockets/TractorSensorDataGenerator.java
@@ -1,27 +1,23 @@
 package org.syngenta.trainings.websockets;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.syngenta.trainings.websockets.domain.TractorSensorData;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Service
 public class TractorSensorDataGenerator {
-    private static final int NUMBER_OF_TRACTORS = 5;
-
+    @Value("${application.number-of-tractors}")
+    private int numberOfTractors;
     private List<TractorSimulation> simulations;
-
-    public TractorSensorDataGenerator() {
-        this.simulations = new ArrayList<>();
-    }
 
     @PostConstruct
     public void init() {
-        simulations = IntStream.range(1, NUMBER_OF_TRACTORS)
+        simulations = IntStream.range(1, numberOfTractors)
                 .mapToObj(i -> new TractorSimulation())
                 .collect(Collectors.toList());
     }
